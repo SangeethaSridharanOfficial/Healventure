e6af9209ad6834f9eef22adfd6c96d52e00c36ae
SangeethaSridharan <sangeethasridharan93@gmail.com>
2021-11-18 12:31:23 -0500

    Resolved conflicts

--- app/src/main/java/com/example/healventureapp/view/Login.java
+++ app/src/main/java/com/example/healventureapp/view/Login.java
@@ -4,23 +4,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.healventureapp.MainActivity;
 import com.example.healventureapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
+    private DatabaseReference lDatabase;
+    private DatabaseReference loginEndPoint;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
+        lDatabase = FirebaseDatabase.getInstance().getReference();
+        loginEndPoint = lDatabase.child("login");
+        Toast.makeText(this, "Firebase Connected", Toast.LENGTH_LONG).show();
     }
 
     public void loginClicked(View view) {
@@ -28,6 +32,11 @@ public class Login extends AppCompatActivity {
 //        Intent openPage = new Intent(Intent.ACTION_SEND);
 //        if (openPage.resolveActivity(getPackageManager()) != null) {
 //            startActivity(openPage);
-
-        }
+        loginEndPoint.setValue("Test").addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        });
+    }
 }
